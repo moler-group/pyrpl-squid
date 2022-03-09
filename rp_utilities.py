c84fe848f23eec4d89d38042de176ab45c0f4391
Logan Bishop-Van Horn <logan.bvh@gmail.com>
2022-03-09 09:32:49 -0800

    cleanup

--- rp_utilities.py
+++ rp_utilities.py
@@ -7,16 +7,14 @@ def get_keypress():
     return ord(msvcrt.getch()) if msvcrt.kbhit() else 0
 
 
-def get_gui():
-    return QtWidgets.QApplication.instance()
+def relock_squid(rp, array_max=0.05, delay=1e-3, enable_gui=True):
+    """Attempts to detect if the SQUID is unlocked and relock if necessary (Windows only).
 
-
-def relock_squid(rp, array_max=0.05, delay=1e-3):
-    """ Attempts to detect if the SQUID is unlocked and relock if necessary. Windows only.
-        Inputs:
-            - rp: Red Pitaya object as defined by rp = p.rp, where p = Pyrpl('squid_lockbox')
-            - array_max: threshold for the absolute value of array voltage to detect unlocking
-            - delay: time delay in seconds between subsequent loops
+    Args:
+        rp: Red Pitaya object as defined by rp = p.rp, where p = Pyrpl('squid_lockbox').
+        array_max (float): Threshold for the absolute value of array voltage to detect unlocking.
+        delay (float): Time delay in seconds between subsequent loops.
+        enable_gui (bool): If True, allow the GUI to continue running while the re-lock loop is running.
     """
 
     msg = [
@@ -26,19 +24,23 @@ def relock_squid(rp, array_max=0.05, delay=1e-3):
     ]
     print("\n".join(msg))
 
+    gui = QtWidgets.QApplication.instance()
+
     while True:
         try:
-            if get_keypress() == 113: # q
+            if get_keypress() == 113: # User pressed "q"
                 raise KeyboardInterrupt
-            if abs(rp.pid0.ival) > 3.98 or abs(rp.scope.voltage_in2 - rp.pid0.setpoint) > array_max:
+            if (
+                abs(rp.pid0.ival) > 3.98 # Integrator has railed
+                or abs(rp.scope.voltage_in2 - rp.pid0.setpoint) > array_max # Lock was lost
+            ):
                 print("Relocking SQUID...")
                 rp.pid0.ival = 0
-                time.sleep(delay * 5)
+                time.sleep(delay * 10)
                 print(f"New lockpoint: Mod V = {rp.scope.voltage_in1}, Array V = {rp.scope.voltage_in2}.")
                 print(msg[-1])
                 print("-" * 40)
-            gui = get_gui()
-            if gui is not None:
+            if enable_gui and gui is not None:
                 gui.processEvents()
             time.sleep(delay)
         except KeyboardInterrupt:
